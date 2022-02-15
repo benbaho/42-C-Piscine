8c13247ba9390017cc5203f29659a51bd6287a5b
Baho <95172971+benbaho@users.noreply.github.com>
2022-02-15 10:52:30 +0300

    Add files via upload
    %85

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdurmus <bdurmus@student.42kocaeli.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 11:19:21 by bdurmus           #+#    #+#             */
+/*   Updated: 2021/12/11 22:32:24 by bdurmus           ###   ########.tr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	char	a;
+	char	b;	
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1 ;
+		while (b <= 99)
+		{
+			ft_putchar(a / 10 + '0');
+			ft_putchar(a % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			if (a < 98)
+			{	
+				write(1, ", ", 2);
+			}	
+			b++ ;
+		}
+		a++;
+	}
+}
